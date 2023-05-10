703691ba1f8f3070c34b19e013c17c9e6ce0132b
pttm <45825650+potatomi@users.noreply.github.com>
2023-05-10 16:15:06 -0400

    should be working

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ string promptSpecial();
 
 int main()
 {
-    // regexMain();
+    regexMain();
 
     // test code goes here.
 
@@ -42,32 +42,29 @@ string makeRegex()
     string finalRegex = "(";
     bool exitCurrentProcess = false;
     // for user inputs
-    int intInput;
+    int intInput, intInput1, intInput2;
     char charInput;
     string newPattern;
     while (!exitCurrentProcess)
     {
         cout << "Your current regex is: \\" << finalRegex << "\\" << endl;
 
-        // TODO: prompt user to...
-        // enter what type of input
-        // literal/range/special
         cout << "What type of string do you want next? "
-             << "\n\t1 - exact string(literal);\n\t2 - match one of the chars;\n\t3 - match a range of chars; \n\t4 - special;"
+             << "\n\t1 - exact string(literal);\n\t2 - match one of the chars;\n\t3 - match a range of chars; \n\t4 - special(or raw regex);"
              << endl;
         cin >> intInput;
+        cin.ignore();
 
         // support for () groups will be added later
         // probably with recursion
 
-        // TODO: switch statements
         switch (intInput)
         {
         case 1:
             newPattern = promptLiteral();
             break;
         case 2:
-            newPattern = promptRange();
+            newPattern = promptSet();
             break;
         case 3:
             newPattern = promptRange();
@@ -80,8 +77,41 @@ string makeRegex()
         }
         finalRegex += newPattern;
 
-        // TODO: prompt a detailed question
         // TODO: prompt for repititions
+        cout << "Okay, you just added \\" << newPattern << "\\ to your regex\n"
+             << "How many times do you want it to repeat?\n"
+             << "\n\t1 - once;\n\t2 - a specific number of times;\n\t3 - a range of numbers; \n\t4 - at least n, but up to infinity;"
+             << endl;
+        cin >> intInput;
+        newPattern = "";
+
+        switch (intInput)
+        {
+        case 1:
+            break;
+        case 2:
+            cout << "enter how many times you want to repeat:\n"
+                 << endl;
+            cin >> intInput1;
+            newPattern = "{" + to_string(intInput1) + "}";
+            break;
+        case 3:
+            cout << "enter how many times you want to repeat, separated by spaces:\n"
+                 << endl;
+            cin >> intInput1 >> intInput2;
+            newPattern = "{" + to_string(intInput1) + "," + to_string(intInput2) + "}";
+            break;
+        case 4:
+            cout << "enter how many times you want to repeat (at least):\n"
+                 << endl;
+            cin >> intInput1;
+            newPattern = "{" + to_string(intInput1) + ",}";
+            break;
+        default:
+            cout << "Hmm, I'm not sure if you entered something legit...\n"
+                 << "I'll default to the pattern occuring only once.\n";
+        }
+        finalRegex += newPattern;
 
         // TODO: ask user if an "or" is needed in this sequence
         // if yes, add | to the regex
@@ -89,6 +119,29 @@ string makeRegex()
         // and ask if anything else should be concatenated after
         // if yes, add (
         // if not, exit flag = true
+        cout << "do you want an \'or\' in this sequence? (y/n)" << endl;
+        cin >> charInput;
+        switch (charInput)
+        {
+        case 'y':
+            finalRegex += "|";
+            continue;
+
+        default:
+            finalRegex += ")";
+            break;
+        }
+        cout << "do you want to concatenate something new? \nAnswering no would end regex creation. (y/n)" << endl;
+        cin >> charInput;
+        switch (charInput)
+        {
+        case 'y':
+            finalRegex += "(";
+            break;
+
+        default:
+            exitCurrentProcess = true;
+        }
     }
     return finalRegex;
 }
@@ -186,10 +239,32 @@ string promptLiteral()
 
 string promptSet()
 {
+    cout << "enter the characters that you want to match, case sensitive\n"
+         << "don't add spaces in between - spaces are characters as well!\n";
+    string inputLine = "";
+    getline(cin, inputLine);
+    inputLine = autoEscape(inputLine);
+    return "[" + inputLine + "]";
 }
 string promptRange()
 {
+    cout << "enter the range of characters you want, separated by spaces\n";
+    char c1, c2;
+    cin >> c1 >> c2;
+    cin.ignore();
+    if (c1 - c2 > 0)
+    {
+        cout << "Larger char detected in 1st input - reversing...";
+        return "[" + to_string(c2) + "-" + to_string(c1) + "]";
+    }
+    return "[" + to_string(c1) + "-" + to_string(c2) + "]";
 }
 string promptSpecial()
 {
+    cout << "enter whatever regex you want\n"
+         << R"(special characters won't be escaped, so you can input stuff like \n, \t for newlines and tabs)"
+         << endl;
+    string inputLine = "";
+    getline(cin, inputLine);
+    return inputLine;
 }
